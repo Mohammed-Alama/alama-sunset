b5f8946af87ad192bea8433583711ba68abce98e
Mohammed-Alama <mohammedalsayed9688@gmail.com>
2019-09-16 04:39:00 +0200

    Add Caption to Gallery Format

--- template-parts/content-gallery.php
+++ template-parts/content-gallery.php
@@ -4,15 +4,18 @@
 <article id="post<?php the_ID()?>" <?php post_class('sunset-format-gallery'); ?>>
     <?php if(sunset_get_attachment()): $attachments = sunset_get_attachment(4);?>
         <div id="post-gallery-<?php the_ID();?>" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <?php $i = 0; foreach ($attachments as $attachment):$active =  ($i == 0 ? 'active' : '') ?>
-                <li data-target="#post-gallery-<?php the_ID();?>" data-slide-to="<?php echo $i;?>" class="<?php echo $active;?>"></li>
-                <?php $i++;endforeach;?>
-            </ol>
+<!--            <ol class="carousel-indicators">-->
+<!--                --><?php //$i = 0; foreach ($attachments as $attachment):$active =  ($i == 0 ? 'active' : '') ?>
+<!--                <li data-target="#post-gallery---><?php //the_ID();?><!--" data-slide-to="--><?php //echo $i;?><!--" class="--><?php //echo $active;?><!--"></li>-->
+<!--                --><?php //$i++;endforeach;?>
+<!--            </ol>-->
             <div class="carousel-inner">
                 <?php $i = 0; foreach ($attachments as $attachment):$active =  ($i == 0 ? 'active' : '') ?>
                     <div class="carousel-item <?php echo $active; ?> ">
                         <img class="standard-featured  d-block w-100" src="<?php echo wp_get_attachment_url($attachment->ID);?>" alt="">
+                        <div class="entry-excerpt image-caption">
+                            <p><?php echo $attachment->post_excerpt;?></p>
+                        </div>
                     </div>
                 <?php $i++;endforeach;?>
             </div>
